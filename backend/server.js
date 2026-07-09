570b529eb5dd3ceea1b07f9b4b708e2fbbb17bd6
Divyansh Gupta <divyansh.gupta.min24@itbhu.ac.in>
2026-07-09 23:18:05 +0530

    Add backend endpoint

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('SyncBoard Backend is Running 🚀');
+});
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: { origin: '*' },
